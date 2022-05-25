9682a10ad5cd0d95c8c24a11f6298ef7fe665a2e
Mariana Camargo Rubim <mari.c.rubim921@gmail.com>
2022-05-25 16:49:15 -0300

    sass alterado | mínimas mudanças feitas no login

--- sql/script.sql
+++ sql/script.sql
@@ -1,45 +1,203 @@
-CREATE DATABASE IF NOT EXISTS legado DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_0900_ai_ci;
-USE legado;
+-- MySQL Workbench Forward Engineering
 
--- DROP TABLE IF EXISTS perfil;
-CREATE TABLE perfil (
-  id int NOT NULL AUTO_INCREMENT,
-  nome varchar(50) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY nome_UN (nome)
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- Manter sincronizado com enums/perfil.ts e models/perfil.ts
-INSERT INTO perfil (nome) VALUES ('Administrador'), ('Comum');
+-- -----------------------------------------------------
+-- Schema litag
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `litag` ;
 
--- DROP TABLE IF EXISTS usuario;
-CREATE TABLE usuario (
-  id int NOT NULL AUTO_INCREMENT,
-  email varchar(100) NOT NULL,
-  nome varchar(100) NOT NULL,
-  idperfil int NOT NULL,
-  senha varchar(100) NOT NULL,
-  token char(32) DEFAULT NULL,
-  exclusao datetime NULL,
-  criacao datetime NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY usuario_email_UN (email),
-  KEY usuario_exclusao_IX (exclusao),
-  KEY usuario_idperfil_FK_IX (idperfil),
-  CONSTRAINT usuario_idperfil_FK FOREIGN KEY (idperfil) REFERENCES perfil (id) ON DELETE RESTRICT ON UPDATE RESTRICT
-);
+-- -----------------------------------------------------
+-- Schema litag
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `litag` DEFAULT CHARACTER SET utf8 ;
+USE `litag` ;
 
-INSERT INTO usuario (email, nome, idperfil, senha, token, criacao) VALUES ('admin@meutestamento.com.br', 'Administrador', 1, 'peTcC99vkvvLqGQL7mdhGuJZIvL2iMEqvCNvZw3475PJ:JVyo1Pg2HyDyw9aSOd3gNPT30KdEyiUYCjs7RUzSoYGN', NULL, NOW());
+-- -----------------------------------------------------
+-- Table `litag`.`Perfil`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `litag`.`Perfil` ;
 
--- DROP TABLE IF EXISTS conteudo;
-CREATE TABLE conteudo (
-  id int NOT NULL AUTO_INCREMENT,
-  idusuario int NOT NULL,
-  nome varchar(100) NOT NULL,
-  descricao mediumtext NOT NULL,
-  criacao datetime NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY conteudo_nome_UN (nome),
-  KEY conteudo_idusuario_FK_IX (idusuario),
-  CONSTRAINT conteudo_idusuario_FK FOREIGN KEY (idusuario) REFERENCES usuario (id) ON DELETE CASCADE ON UPDATE RESTRICT
-);
+CREATE TABLE IF NOT EXISTS `litag`.`Perfil` (
+  `id_perfil` INT NOT NULL,
+  `nome_perfil` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_perfil`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `litag`.`Usuario`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `litag`.`Usuario` ;
+
+CREATE TABLE IF NOT EXISTS `litag`.`Usuario` (
+  `id_usuario` INT NOT NULL,
+  `senha` VARCHAR(45) NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `ultima_entrada` DATETIME NOT NULL,
+  `Perfil_id_perfil` INT NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  INDEX `fk_Usuario_Perfil1_idx` (`Perfil_id_perfil` ASC) VISIBLE,
+  CONSTRAINT `fk_Usuario_Perfil1`
+    FOREIGN KEY (`Perfil_id_perfil`)
+    REFERENCES `litag`.`Perfil` (`id_perfil`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `litag`.`Contato`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `litag`.`Contato` ;
+
+CREATE TABLE IF NOT EXISTS `litag`.`Contato` (
+  `id_contato` INT NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `Usuario_id_usuario` INT NOT NULL,
+  PRIMARY KEY (`id_contato`),
+  INDEX `fk_Contatos_Usuario1_idx` (`Usuario_id_usuario` ASC) VISIBLE,
+  CONSTRAINT `fk_Contatos_Usuario1`
+    FOREIGN KEY (`Usuario_id_usuario`)
+    REFERENCES `litag`.`Usuario` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = ujis;
+
+
+-- -----------------------------------------------------
+-- Table `litag`.`Categoria_Documento`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `litag`.`Categoria_Documento` ;
+
+CREATE TABLE IF NOT EXISTS `litag`.`Categoria_Documento` (
+  `id_tipo_documento` INT NOT NULL,
+  `desc_tipo_documento` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_tipo_documento`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `litag`.`Documento`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `litag`.`Documento` ;
+
+CREATE TABLE IF NOT EXISTS `litag`.`Documento` (
+  `id_documento` INT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `desc_documento` VARCHAR(45) NOT NULL,
+  `Dim_Tipo_Documentos_id_tipo_documentos` INT NOT NULL,
+  `Usuario_id_usuario` INT NOT NULL,
+  `conteudo_documento` MEDIUMTEXT NULL,
+  `extensao_documento` VARCHAR(45) NULL,
+  PRIMARY KEY (`id_documento`),
+  INDEX `fk_Dim_Documentos_Dim_Tipo_Documentos1_idx` (`Dim_Tipo_Documentos_id_tipo_documentos` ASC) VISIBLE,
+  INDEX `fk_Documentos_Usuario1_idx` (`Usuario_id_usuario` ASC) VISIBLE,
+  CONSTRAINT `fk_Dim_Documentos_Dim_Tipo_Documentos1`
+    FOREIGN KEY (`Dim_Tipo_Documentos_id_tipo_documentos`)
+    REFERENCES `litag`.`Categoria_Documento` (`id_tipo_documento`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Documentos_Usuario1`
+    FOREIGN KEY (`Usuario_id_usuario`)
+    REFERENCES `litag`.`Usuario` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `litag`.`Autorizados`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `litag`.`Autorizados` ;
+
+CREATE TABLE IF NOT EXISTS `litag`.`Autorizados` (
+  `id_autorizados` INT NOT NULL,
+  PRIMARY KEY (`id_autorizados`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `litag`.`Documentos_has_Autorizados`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `litag`.`Documentos_has_Autorizados` ;
+
+CREATE TABLE IF NOT EXISTS `litag`.`Documentos_has_Autorizados` (
+  `Documentos_id_documentos` INT NOT NULL,
+  `Documentos_Usuario_id_usuario` VARCHAR(45) NOT NULL,
+  `Autorizados_id_autorizados` INT NOT NULL,
+  PRIMARY KEY (`Documentos_id_documentos`, `Documentos_Usuario_id_usuario`, `Autorizados_id_autorizados`),
+  INDEX `fk_Documentos_has_Autorizados_Autorizados1_idx` (`Autorizados_id_autorizados` ASC) VISIBLE,
+  INDEX `fk_Documentos_has_Autorizados_Documentos1_idx` (`Documentos_id_documentos` ASC, `Documentos_Usuario_id_usuario` ASC) VISIBLE,
+  CONSTRAINT `fk_Documentos_has_Autorizados_Documentos1`
+    FOREIGN KEY (`Documentos_id_documentos`)
+    REFERENCES `litag`.`Documento` (`id_documento`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Documentos_has_Autorizados_Autorizados1`
+    FOREIGN KEY (`Autorizados_id_autorizados`)
+    REFERENCES `litag`.`Autorizados` (`id_autorizados`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `litag`.`Contato x Documento`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `litag`.`Contato x Documento` ;
+
+CREATE TABLE IF NOT EXISTS `litag`.`Contato x Documento` (
+  `id_contato_documento` BIGINT NOT NULL,
+  `Contato_id_contato` INT NOT NULL,
+  `Documento_id_documento` INT NOT NULL,
+  INDEX `fk_Contatos_has_Documentos_Documentos1_idx` (`Documento_id_documento` ASC, `id_contato_documento` ASC) VISIBLE,
+  INDEX `fk_Contatos_has_Documentos_Contatos1_idx` (`Contato_id_contato` ASC) VISIBLE,
+  PRIMARY KEY (`id_contato_documento`),
+  CONSTRAINT `fk_Contatos_has_Documentos_Contatos1`
+    FOREIGN KEY (`Contato_id_contato`)
+    REFERENCES `litag`.`Contato` (`id_contato`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Contatos_has_Documentos_Documentos1`
+    FOREIGN KEY (`Documento_id_documento`)
+    REFERENCES `litag`.`Documento` (`id_documento`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = ujis;
+
+
+-- -----------------------------------------------------
+-- Table `litag`.`Contato x Categoria_Documento`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `litag`.`Contato x Categoria_Documento` ;
+
+CREATE TABLE IF NOT EXISTS `litag`.`Contato x Categoria_Documento` (
+  `id_contato_categoria` BIGINT NOT NULL,
+  `Categoria_Documento_id_tipo_documento` INT NOT NULL,
+  `Contato_id_contato` INT NOT NULL,
+  INDEX `fk_Categoria_Documento_has_Contato_Contato1_idx` (`Contato_id_contato` ASC) VISIBLE,
+  INDEX `fk_Categoria_Documento_has_Contato_Categoria_Documento1_idx` (`Categoria_Documento_id_tipo_documento` ASC) VISIBLE,
+  PRIMARY KEY (`id_contato_categoria`),
+  CONSTRAINT `fk_Categoria_Documento_has_Contato_Categoria_Documento1`
+    FOREIGN KEY (`Categoria_Documento_id_tipo_documento`)
+    REFERENCES `litag`.`Categoria_Documento` (`id_tipo_documento`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Categoria_Documento_has_Contato_Contato1`
+    FOREIGN KEY (`Contato_id_contato`)
+    REFERENCES `litag`.`Contato` (`id_contato`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
